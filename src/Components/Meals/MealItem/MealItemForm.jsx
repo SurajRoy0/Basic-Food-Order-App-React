af7180203378136463f1d9f3667eddb3f89d7800
SurajRoy0 <myself.suraj0@gmail.com>
2023-04-29 23:58:35 +0530

    Modal added

--- src/Components/Meals/MealItem/MealItemForm.jsx
+++ src/Components/Meals/MealItem/MealItemForm.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
-const MealItemForm = () => {
+
+const MealItemForm = (props) => {
   return (
-    <div className={classes.form}>
-      <div>
-        <span>Amount</span>
-        <input type="number" />
-      </div>
-      <div>
-        <button>+Add</button>
-      </div>
-    </div>
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
   );
 };
 
